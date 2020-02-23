bb3cc4156a81f876711cae6d5fadf2bedc5908b9
GrothDK <61182782+GrothDK@users.noreply.github.com>
2020-02-23 19:44:54 +0100

    edit produkter

--- produktside.php
+++ produktside.php
@@ -18,10 +18,10 @@
 
 
 <div class="container main-content">
-  <div class="container">
+  <div class="container produkter">
   <h1 class='big-h1'>PRODUKTER</h1>
-<div class="card-deck udvalgte-produkter">
-  <div class="card h-50">
+<div class="card-deck">
+  <div class="card">
     <img class="card-img-top" src="images/mel2.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Økologisk hvedemel</h5>
@@ -48,6 +48,34 @@
       <a href="#" class="fake-btn">LÆG I KURV</a>
     </div>
   </div>
+<div class="card">
+    <img class="card-img-top" src="images/provoiant-rapsolie-citron.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Økologisk rapsolie</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer... <a href="" class="read-more">Læs mere</a></p>
+      <p class='price'>59,95 kr</p>
+      <a href="#" class="fake-btn">LÆG I KURV</a>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="images/provoiant-rapsolie-citron.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Økologisk rapsolie</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer... <a href="" class="read-more">Læs mere</a></p>
+      <p class='price'>59,95 kr</p>
+      <a href="#" class="fake-btn">LÆG I KURV</a>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="images/provoiant-rapsolie-citron.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Økologisk rapsolie</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer... <a href="" class="read-more">Læs mere</a></p>
+      <p class='price'>59,95 kr</p>
+      <a href="#" class="fake-btn">LÆG I KURV</a>
+    </div>
+  </div>
+
 </div>
 
 </div>
