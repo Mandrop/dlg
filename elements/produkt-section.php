f37811b8c14e890f53ea45d69d0032d71cdb74e1
GrothDK <61182782+GrothDK@users.noreply.github.com>
2020-02-24 13:11:03 +0100

    produkt section Update
    Der er lavet en while løkke

--- elements/produkt-section.php
+++ elements/produkt-section.php
@@ -9,9 +9,13 @@
     $sql = "SELECT * FROM products";
     $sqlQuery = $db->query($sql);
 
-    if($sqlQuery){
+/*     if($sqlQuery){
         $dbFetch = $sqlQuery->fetch_object();
-    }
+
+    } */
+
+
+
 
 ?>
 
@@ -19,60 +23,24 @@
   <div class="container produkter">
   <h1 class='big-h1'>PRODUKTER</h1>
 <div class="card-deck">
-  <div class="card">
-    <img class="card-img-top" src="images/mel2.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title"><?php echo "$dbFetch->product_title"; ?>Økologisk hvedemel</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer... <a href="" class="read-more">Læs mere</a></p>
-      <p class='price'>29,95 kr</p>
-      <a href="#" class="fake-btn">LÆG I KURV</a>
+  
+  <?php
+//UDSKRIV PRODUKTERNE TIL SIDEN
+     while($dbFetch = $sqlQuery->fetch_object()){
+        echo "
+  <div class='card'>
+    <img class='card-img-top' src='$dbFetch->product_image' alt='Card image cap'>
+    <div class='card-body'>
+      <h5 class='card-title'> $dbFetch->product_title </h5>
+      <p class='card-text'>$dbFetch->product_content ... <a href='' class='read-more'>Læs mere</a></p>
+      <p class='price'>$dbFetch->product_price kr.</p>
+      <a href='' class='fake-btn'>LÆG I KURV</a>
     </div>
   </div>
-  <div class="card">
-    <img class="card-img-top" src="images/oel2.png" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Økologisk rug øl</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer... <a href="" class="read-more">Læs mere</a></p>
-      <p class='price'>39,95 kr</p>
-      <a href="#" class="fake-btn">LÆG I KURV</a>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="images/provoiant-rapsolie-citron.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Økologisk rapsolie</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer... <a href="" class="read-more">Læs mere</a></p>
-      <p class='price'>59,95 kr</p>
-      <a href="#" class="fake-btn">LÆG I KURV</a>
-    </div>
-  </div>
-<div class="card">
-    <img class="card-img-top" src="images/provoiant-rapsolie-citron.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Økologisk rapsolie</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer... <a href="" class="read-more">Læs mere</a></p>
-      <p class='price'>59,95 kr</p>
-      <a href="#" class="fake-btn">LÆG I KURV</a>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="images/provoiant-rapsolie-citron.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Økologisk rapsolie</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer... <a href="" class="read-more">Læs mere</a></p>
-      <p class='price'>59,95 kr</p>
-      <a href="#" class="fake-btn">LÆG I KURV</a>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="images/provoiant-rapsolie-citron.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Økologisk rapsolie</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer... <a href="" class="read-more">Læs mere</a></p>
-      <p class='price'>59,95 kr</p>
-      <a href="#" class="fake-btn">LÆG I KURV</a>
-    </div>
-  </div>
+        ";
+    } 
+
+  ?>
 
 </div>
 
@@ -80,3 +48,18 @@
   </div>
 
 <!-- PRODUKTSIDE -->
+
+<!-- BACKUP -->
+
+<!-- 
+  <div class="card">
+    <img class="card-img-top" src="images/mel2.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo "$dbFetch->product_title"; ?>Økologisk hvedemel</h5>
+      <p class="card-text"><?php echo "$dbFetch->product_content"; ?>... <a href="" class="read-more">Læs mere</a></p>
+      <p class='price'><?php echo "$dbFetch->product_price"; ?> kr</p>
+      <a href="#" class="fake-btn">LÆG I KURV</a>
+    </div> 
+    -->
+
+<!-- BACKUP -->
