eb1adf622edb867161157b0e69a069cf324d61ac
GrothDK <61182782+GrothDK@users.noreply.github.com>
2020-02-24 10:25:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include('config.php');
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="css/style.css">
+    
 
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700|Oswald:300,400,500&display=swap" rel="stylesheet">
 
