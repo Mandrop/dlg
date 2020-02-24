257fe6cb4bf293d8dfe596b0a2cba16f1e3c4959
GrothDK <61182782+GrothDK@users.noreply.github.com>
2020-02-24 09:15:14 +0100

    include footer

--- index.php
+++ index.php
@@ -106,40 +106,12 @@ include('elements/news-section.php')
 
 
     <div class="container"></div>
-    <footer>
-      <div class="container contact-info">
-  <div class="row">
-    <div class="col">
-      <span class='fake-heading'><p>Adresse</p></span>
-      <p>Den lille gårdbutik</p>
-      <p>Gårdvej 1</p>
-      <p>Viborg 8800</p>
-    </div>
-    <div class="col-6">
-      <span class='fake-heading'><p>Kontakt</p></span>
-      <p>11 22 33 44</p>
-      <p>denlillegaardbutik@gmail.dk</p>
-    </div>
-    <div class="col">
-      <span class='fake-heading'><p>Åbningstider</p></span>
-      <p>Mandag - Torsdag   09:00 - 17:30</p>
-      <p>Fredag   09:00 - 18:00</p>
-      <p>Lørdag - Søndag  10:00 - 13:00</p>
-    </div>
-  </div>
-</div>
+    <!-- FOOTER START -->
+    <?php
+  include('elements/footer.php')
+    ?>
+ <!-- FOOTER END -->
 
-<hr class='hr-white'>
-
-<form method='post'>
-
-  <p class='big-paragraph'>Send os en besked</p>
-  <input class='d-block' type="text" placeholder='Navn' name='formName'></input>
-  <input class='d-block' type='email' placeholder='Email' name='formEmail'></input>
-  <textarea class='d-block' name="formText" placeholder='Besked' id="" cols="30" rows="10"></textarea>
-  <input class='d-block submit' type="submit" value='Send'>
-</form>
-    </footer>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
