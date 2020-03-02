1a283c4213f4cbf198c74bef4aa67a1d75f780b1
GrothDK <61182782+GrothDK@users.noreply.github.com>
2020-03-02 10:27:36 +0100

    edit

--- admin/config.php
+++ admin/config.php
@@ -1,5 +1,6 @@
 <?php
 
 $db = new mysqli("localhost", "root", "", "dlg");
+/* $db = new mysqli("hengro.dk.mysql", "hengro_dk", "Denlillegaardbutik", "hengro_dk"); */
 
 ?>
--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 
 $db = new mysqli("localhost", "root", "", "dlg");
-
+/* $db = new mysqli("hengro.dk.mysql", "hengro_dk", "Denlillegaardbutik", "hengro_dk"); */
 ?>
+
+
